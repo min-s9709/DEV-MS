cd183d58e522af16274c2dcbb4261e059e783336
min-s9709 <mim970929@naver.com>
2024-06-05 15:38:52 +0900

    전역 폰트 및 테마 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'size-big-title': '1.5rem',
+        'size-medium-title': '1.125rem',
+        'size-body': '1rem',
+        'size-subbody': '0.75rem',
+      },
+      colors: {
+        'dark-gray-color': '#777777',
+        'medium-gray-color': '#00000080',
+        'light-gray-color': '#E5E5E5',
+        'c-green-color': '#12B886',
+        'c-light-color': '#F8F9FA',
+      },
+    },
   },
   plugins: [],
 }
