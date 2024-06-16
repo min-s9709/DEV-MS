af3b55a795feebcc934e4abdc88a297f9c9cfe49
min-s9709 <mim970929@naver.com>
2024-06-16 18:55:15 +0900

    feat: 블로그 배너

---
+++ src/components/blog/Banner.tsx
@@ -0,0 +1,26 @@
+import { StaticImage } from 'gatsby-plugin-image'
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <div className={'relative w-full h-80'}>
+      <StaticImage
+        src='../../images/blogBanner.jpg'
+        alt='blog-banner'
+        className={'object-cover w-full h-full opacity-90'}
+      ></StaticImage>
+      <div className={'absolute top-1/2 left-1/3 right-1/3 text-center'}>
+        <h1 className={'font-extrabold text-size-big-title text-white'}>
+          김민성의 개발 블로그
+        </h1>
+        <h3
+          className={
+            'font-bold text-size-medium-title text-light-gray-color mt-6'
+          }
+        >
+          개발하면서 겪는 다양한 경험들을 기록하는 공간입니다.
+        </h3>
+      </div>
+    </div>
+  )
+}
