067fdff8c418a49d7158ca13beba917226ffb78a
min-s9709 <mim970929@naver.com>
2024-06-25 16:08:10 +0900

    feat: 카테고리 별 post 수 추가

--- src/components/blog/category/CategoryHeader.tsx
+++ src/components/blog/category/CategoryHeader.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 interface ICategoryHeader {
   category?: string
+  count: number
 }
 
-export default function CategoryHeader({ category }: ICategoryHeader) {
+export default function CategoryHeader({ category, count }: ICategoryHeader) {
   return (
     <div className={'w-full border-b-[1px] p-2'}>
       <div className={'flex justify-center'}>
         <h1 className={'text-size-big-title mr-4'}>
           {category ? category : 'All'}
         </h1>
-        <h1 className={'text-size-big-title text-[#DA0909]'}>8</h1>
+        <h1 className={'text-size-big-title text-[#DA0909]'}>{count}</h1>
       </div>
     </div>
   )
