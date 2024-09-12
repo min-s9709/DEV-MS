cf1ba844b875cf5ddb985f96b66741a0d537af86
min-s9709 <mim970929@naver.com>
2024-09-12 15:28:54 +0900

    feat: home 화면 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Layout from '../components/common/Layout'
 import Seo from '../components/seo/Seo'
-import ProfileCard from '../components/profileCard/ProfileCard'
+import ProfileCard from '../components/home/profileCard/ProfileCard'
+import About from '../components/home/about/About'
+import HomeProject from '../components/home/projects/HomeProject'
+import HomeRecentPosts from '../components/home/posts/HomeRecentPosts'
+import Section from '../components/home/section/Section'
 
 export default function Home() {
   return (
     <Layout>
-      <div className='flex items-center max-w-screen-lg'>
-        <ProfileCard />
+      <div className='w-full mx-auto'>
+        <Section type='main'>
+          <ProfileCard />
+        </Section>
+        <Section>
+          <About />
+        </Section>
+        <Section>
+          <HomeProject />
+        </Section>
+        <Section>
+          <HomeRecentPosts />
+        </Section>
       </div>
     </Layout>
   )
